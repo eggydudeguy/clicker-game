1ba1563808f27734bb61eabc3bfc2dc0a01b6032
eggydudeguy <148922500+eggydudeguy@users.noreply.github.com>
2026-01-06 20:05:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let count = 0;
+const countDisplay = document.getElementById("count");
+const button = document.getElementById("clickBtn");
+
+button.addEventListener("click", () => {
+  count++;
+  countDisplay.textContent = count;
+});
 
